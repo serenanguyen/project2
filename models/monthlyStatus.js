52359a9016979a1f2d9c46a23ba71fe567444617
Your Name <s.nguyen11@utexas.edu>
2017-05-01 14:19:38 -0500

    connected to local host server

---
+++ models/monthlyStatus.js
@@ -0,0 +1,15 @@
+module.exports = function(sequelize, DataTypes){
+  var monthlyStatus = sequelize.define("monthlyStatus", {
+    location1 DataTypes.BOOLEAN,
+    location2 DataTypes.BOOLEAN,
+    location3 DataTypes.BOOLEAN,
+    location4 DataTypes.BOOLEAN,
+    location5 DataTypes.BOOLEAN,
+    location6 DataTypes.BOOLEAN,
+    location7 DataTypes.BOOLEAN,
+    location8 DataTypes.BOOLEAN,
+    location9 DataTypes.BOOLEAN,
+    location10 DataTypes.BOOLEAN
+  });
+  return monthlyStatus;
+});
--- server.js
+++ server.js
@@ -1,11 +1,17 @@
-
 var express = require("express");
+var bodyParser = require("body-parser");
 var db = require("./models");
 
 var PORT = 3000;
 var app = express();
 
-//sync models to db before connecting to database so server won't start if there is an error connecting to db or before db is ready  
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+//sync models to db before connecting to database so server won't start if there is an error connecting to db or before db is ready
 db.sequelize.sync().then(function(){
   app.listen(PORT, function(){
     console.log("Listening on port " + PORT);
