0e2373cf211a4f8f8472b47120348e7f21a4c9ab
Your Name <s.nguyen11@utexas.edu>
2017-05-02 16:06:24 -0500

    start of user authentication

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+var passport = require("passport");
+var LocalStrategy = require('passport-local').Strategy;
 
 var app = express();
 var PORT = process.env.PORT || 8080;
@@ -17,8 +19,28 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
-//static dir
+// static dir
 app.use(express.static("./public"));
+
+// init passport
+// app.use(express.session());
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy(
+  function(username, password, done){
+    User.findOne({ username:username }, function(err, user){
+      if(err) { return done(err);}
+      if(!user) {
+        return done(null, false, { message: 'Incorrect username.'});
+      }
+      if(!user.validPassword(password)){
+        return done(null, false, {message: 'Incorrect password.'});
+      }
+      return done(null, user);
+    });
+  }
+));
 
 // routes
 require("./routes/html-routes.js")(app);
