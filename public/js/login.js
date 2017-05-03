1bbc6c279882195c7d6b30c054d43fccae52fc30
Your Name <s.nguyen11@utexas.edu>
2017-05-03 09:16:36 -0500

    passport changes

--- public/js/login.js
+++ public/js/login.js
@@ -1,5 +1,6 @@
 var passport = require("passport");
 var LocalStrategy = require('passport-local').Strategy;
+var User = require("../models/user");
 // init passport
 // app.use(express.session());
 app.use(passport.initialize());
@@ -7,7 +8,7 @@ app.use(passport.session());
 
 passport.use(new LocalStrategy(
   function(username, password, done){
-    User.findOne({ username:username }, function(err, user){
+    User.findOne({ usernameField:username }, function(err, user){
       if(err) { return done(err);}
       if(!user) {
         return done(null, false, { message: 'Incorrect username.'});
