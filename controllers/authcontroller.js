81511c61acd13ebc0d96441636b5731e8d7d6d4b
Your Name <s.nguyen11@utexas.edu>
2017-05-05 13:24:37 -0500

    locations

--- controllers/authcontroller.js
+++ controllers/authcontroller.js
@@ -9,7 +9,7 @@ exports.signin = function(req, res) {
 };
 
 exports.dashboard = function(req, res) {
-    res.render('dashboard');
+    res.render('dashboard', req.user);
 };
 
 exports.logout = function(req, res) {
