134472eb865166b650b8d385d7e61e3e2ffd1574
Your Name <s.nguyen11@utexas.edu>
2017-05-14 12:38:25 -0500

    merging

--- public/js/frontend.js
+++ public/js/frontend.js
@@ -1,4 +1,4 @@
-	 (function($){
+ (function($){
         $(window).on("load",function(){
             $("#displayChecklist").mCustomScrollbar({
             	theme: "rounded-dots"
@@ -30,3 +30,31 @@
             });
         });
     })(jQuery);
+
+
+$("div.check").on("click", function(event){
+
+	$(this).css("background-image", "url(assets/img/checkedBox.png)");
+})
+
+
+
+    $.fn.stars = function() {
+        return $(this).each(function() {
+
+            var rating = $(this).data("rating");
+
+            var numStars = $(this).data("numStars");
+
+            var fullStar = new Array(Math.floor(rating + 1)).join('<i class="fa fa-star"></i>');
+
+            var halfStar = ((rating%1) !== 0) ? '<i class="fa fa-star-half-empty"></i>': '';
+
+            var noStar = new Array(Math.floor(numStars + 1 - rating)).join('<i class="fa fa-star-o"></i>');
+
+            $(this).html(fullStar + halfStar + noStar);
+
+        });
+    }
+
+    $('.stars').stars();
